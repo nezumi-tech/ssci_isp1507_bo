99b36db630cd71a4b5465d5e20da4ad961e2e59a
nezumi_tech <nezumi_tech@yahoo.co.jp>
2021-06-15 10:38:23 +0900

    initial commit

---
+++ variant.cpp
@@ -0,0 +1,81 @@
+/*
+  Copyright (c) 2014-2015 Arduino LLC.  All right reserved.
+  Copyright (c) 2016 Sandeep Mistry All right reserved.
+  Copyright (c) 2018, Adafruit Industries (adafruit.com)
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "variant.h"
+
+#include "wiring_constants.h"
+#include "wiring_digital.h"
+#include "nrf.h"
+
+const uint32_t g_ADigitalPinMap[] = {
+  // D0 - D7
+  0,  // xtal 1
+  1,  // xtal 2
+  2,  // a0	TXD
+  3,  // a1	RXD
+  4,  // a2	SCK
+  5,  // a3	MOSI
+  6,  //	MISO
+  7,  //	SDA
+
+  // D8 - D13
+  8,  //	SCL
+
+  9,  // *NFC1
+  10, // *NFC2
+
+  11, // LED #1 (red)
+
+  12, // *
+  13, // *
+  14, // *
+
+  15, // *GPIO #15
+  16, // *GPIO #16
+
+  // function set pins
+  17, // *
+  18, // *SWO
+  19, // *LED #2 (blue)
+  20, // *DFU
+  21, // Reset
+  22, // *Factory Reset
+  23, // *N/A
+  24, // *N/A
+
+  25, // *
+  26, // *
+  27, // *GPIO #27
+  28, // A4	SPI_SS
+  29, // A5
+  30, // A6
+  31, // *A7
+};
+
+void initVariant()
+{
+  // LED1 & LED2
+  pinMode(PIN_LED1, OUTPUT);
+  ledOff(PIN_LED1);
+
+  pinMode(PIN_LED2, OUTPUT);
+  ledOff(PIN_LED2);
+}
+
